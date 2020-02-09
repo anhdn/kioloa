57a5609b459fb9971becddedd908af0679ed0733
dnanh2011 <dnanh2011@gmail.com>
2020-02-09 19:19:36 +0700

    update code

--- next.config.js
+++ next.config.js
@@ -4,7 +4,41 @@ const withSass = require('@zeit/next-sass')
 // for those who using CDN
 const assetPrefix = ASSET_HOST || ''
 
-module.exports = withSass({
+const withAssetRelocator = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options
+
+      if (isServer) {
+        config.node = Object.assign({}, config.node, {
+          __dirname: false,
+          __filename: false,
+        })
+
+        config.module.rules.unshift({
+          test: /\.(m?js|node)$/,
+          parser: { amd: false },
+          use: {
+            loader: '@zeit/webpack-asset-relocator-loader',
+            options: {
+              outputAssetBase: 'assets',
+              existingAssetNames: [],
+              wrapperCompatibility: true,
+              escapeNonAnalyzableRequires: true,
+            },
+          },
+        })
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+      return config
+    },
+  })
+}
+
+module.exports = withAssetRelocator(withSass({
   assetPrefix,
   target: 'serverless',
   webpack: (config, { dev }) => {
@@ -28,4 +62,4 @@ module.exports = withSass({
 
     return config
   }
-})
+}))
